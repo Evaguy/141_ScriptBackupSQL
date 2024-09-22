23db4ffd8efe8373fe07bb2bad5227792e5b212a
Gabin Chautemps <gabin.chautemps06@gmail.com>
2024-09-22 13:42:58 +0200

    added automation into the script

--- scriptbackup.ps1
+++ scriptbackup.ps1
@@ -12,13 +12,22 @@ $DB_sakila = "sakila"
 $DB_world = "world"
 $DB_menagerie = "menagerie"
 
-# config nom backup
-$BackupFileName = "db_dump_" + (Get-Date -Format "dd_MM_yyyy_HH_mm_ss") + ".sql" 
-
-# backup des dbs root + utilsateur sans mot de passe
-& $mysqldumpPath -u $User --databases $DB_sakila $DB_world $DB_menagerie | Out-File "$Backupfolder\$BackupFileName"
+# config nom backup 
+# + backup des dbs root + utilsateur sans mot de passe
+# + automatisation
+while ($true) {
+    $BackupFileName = "db_dump_" + (Get-Date -Format "dd_MM_yyyy_HH_mm_ss") + ".sql" 
+    & $mysqldumpPath -u $User --databases $DB_sakila $DB_world $DB_menagerie | Out-File "$Backupfolder\$BackupFileName"
+    Write-Host "Le $BackupFilename a bien ete execute"
+    Start-Sleep -Seconds 5
+}
 
 # backup des dbs
 # si l'utilisateur n'est pas root
 # et qu'il a un mot de passe
-# & $mysqldumpPath -u $User --password=$PSW --databases $DB_sakila $DB_world $DB_menagerie | Out-File "$Backupfolder\$BackupFileName"
+# while ($true) {
+    # $BackupFileName = "db_dump_" + (Get-Date -Format "dd_MM_yyyy_HH_mm_ss") + ".sql" 
+    # & $mysqldumpPath -u $User --password=$PSW --databases $DB_sakila $DB_world $DB_menagerie | Out-File "$Backupfolder\$BackupFileName"
+    # Write-Host "Le $BackupFilename a bien ete execute"
+    # Start-Sleep -Seconds 5
+# }
