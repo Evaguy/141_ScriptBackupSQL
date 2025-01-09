4af19a67c51d18a09caa2dee2ffe3ce8352f8ea2
Gabin Chautemps <151857152+Evaguy@users.noreply.github.com>
2025-01-09 16:00:36 +0100

    changed comments just before the loop

--- win/scriptbackup.ps1
+++ win/scriptbackup.ps1
@@ -11,7 +11,7 @@ $DB_3 = ""
 # if you want to backup more dbs, you'll have to create other variable
 # maybe the idea here would be to find a way to only use one variable, but i haven't found anything :/
 
-# backup name + dbs backup config if the user doesn't have a password 
+# backup name + dbs backup config inside of a infinite loop if the user doesn't have a password
 while ($true) {
     $BackupFileName = "db_dump_" + (Get-Date -Format "dd_MM_yyyy_HH_mm_ss") + ".sql" 
     & $mysqldumpPath -u $User --databases $DB_1 $DB_2 $DB_3 | Out-File "$Backupfolder\$BackupFileName"
@@ -19,7 +19,7 @@ while ($true) {
     Start-Sleep -Seconds 5 
 }
 
-# backup name + dbs backup config if the user has a password
+# backup name + dbs backup config inside of a infinite loop if the user has a password
 # while ($true) {
     # $BackupFileName = "db_dump_" + (Get-Date -Format "dd_MM_yyyy_HH_mm_ss") + ".sql" 
     # & $mysqldumpPath -u $User --password=$PSW --databases $DB_1 $DB_2 $DB_3 | Out-File "$Backupfolder\$BackupFileName"
