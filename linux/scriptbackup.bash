890eda0899fd1b47999e25495becd9cbccb8197f
Gabin Chautemps <151857152+Evaguy@users.noreply.github.com>
2025-01-09 15:59:03 +0100

    cleaned a bit the comments inside scriptbackup.bash

--- linux/scriptbackup.bash
+++ linux/scriptbackup.bash
@@ -4,25 +4,23 @@
 user=""
 pswrd="" #if the user has a password, otherwise comment/remove this line
 
-# DBs, add as much as you want
+# DBs to backup
 db1=""
 # db2=""
 # db3=""
+# if you want to backup more dbs, you'll have to create other variable
+# maybe the idea here would be to find a way to only use one variable, but i haven't found anything :/
 
-# Backup Directory
+# Backup dir
 backup_dir="/your/path/to/backup/dir/"
 
-# infinite loop to backup the DB
+# backup name + dbs backup config inside of a infinite loop
 while true
 do
 	# Backup name + Timestamp / day month year hour minutes seconds
 	# need to check for myself if this works because im not sure...
 	BackupFileName="db_dump" + (date +"%d%m%Y%H%M%S") + ".sql"
-	# backup of the DB
-	# to backup multiple DBs, you'll need to add them next to
-	# the other DB (in this example : $db1)
 	mysqldump -u$user -p$pswrd $db1 > $backup_dir/$BackupFileName
-	# message
 	echo "The $BackupFileName was completed"
 	sleep 5
 done
